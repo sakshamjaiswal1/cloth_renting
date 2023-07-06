9855e2fbf478dd92ccf4f8fb6a72ca3e1de8ed91
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2023-07-06 23:00:37 +0530

    adding footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Footer from "./Components/Footer/Footer";
 import SingleProduct from "./Pages/SingleProduct/SingleProduct";
 
 function App() {
   return (
     <div className="">
       <SingleProduct />
+      {/* <Footer /> */}
     </div>
   );
 }
