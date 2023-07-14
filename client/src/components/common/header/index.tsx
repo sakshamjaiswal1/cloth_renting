a27df74828425f92aa50cd0cd17758f3c852d021
sakshamjaiswal1 <sjais973@gmail.com>
2023-07-14 23:01:32 +0530

    header

--- client/src/components/common/header/index.tsx
+++ client/src/components/common/header/index.tsx
@@ -1,20 +1,42 @@
 import cart from "../../../assets/home/cart.svg";
 import Profile from "../../../assets/home/profile";
 import search from "../../../assets/home/search.svg";
+import logo from "../../../assets/home/jainLogo.png";
+import { useEffect, useState } from "react";
 
 function Header({ className }: { className?: string }) {
+  const [isScrolled, setIsScrolled] = useState<boolean>(false);
+
+  const handleScroll = () => {
+    console.log(window.scrollY);
+    if (window.scrollY > 0) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <header
-      className={`${className} flex items-center justify-between w-full pl-[70px] pr-[30px] py-[30px] sticky top-0 z-[1] `}
+      className={`${className} flex items-center justify-between w-full pl-[70px] pr-[30px] py-[30px] sticky top-0 z-[1] ${
+        isScrolled ? "bg-white" : ""
+      }  transition-all ease-in-out duration-300  `}
     >
-      <div className="w-[106px] h-[80px]">
-        <img
-          src="https://cdn.shopify.com/s/files/1/0086/9177/7632/files/Stylease_Final_Logo_1_c463ebee-2285-409d-ad68-9a4d4696c79f_500x.png?v=1613785028"
-          alt=""
-          className="h-full w-full"
-        />
+      <div className="w-[206px] h-[80px]">
+        <img src={logo} alt="" className="h-full w-full" />
       </div>
-      <ul className="flex items-center gap-x-[20px] leading-[25px] text-[14px] tracking-[0px] ">
+      <ul
+        className={`flex items-center gap-x-[20px] leading-[25px] text-[14px] tracking-[0px] ${
+          isScrolled ? "text-black" : "text-white"
+        } `}
+      >
         <li>
           <a href="">WOMENS WEAR </a> <span className={``}></span>
         </li>
@@ -33,15 +55,31 @@ function Header({ className }: { className?: string }) {
       <ul className="flex items-center gap-x-[20px]">
         <li>
           {" "}
-          <Profile className="h-[30px] w-[30px]" />
+          <Profile
+            className={`h-[30px] w-[30px]  ${
+              isScrolled ? "text-black" : "text-white"
+            }`}
+          />
         </li>
         <li>
           {" "}
-          <img src={search} alt="" className="h-[30px] w-[30px]" />{" "}
+          <img
+            src={search}
+            alt=""
+            className={`h-[30px] w-[30px]  ${
+              isScrolled ? "text-black" : "text-white"
+            }`}
+          />{" "}
         </li>
         <li>
           {" "}
-          <img src={cart} alt="" className="h-[30px] w-[30px]" />{" "}
+          <img
+            src={cart}
+            alt=""
+            className={`h-[30px] w-[30px]  ${
+              isScrolled ? "text-black" : "text-white"
+            }`}
+          />
         </li>
       </ul>
     </header>
